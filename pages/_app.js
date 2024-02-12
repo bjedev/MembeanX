4a4e87eff5dbb727f9d5e47b4c02eaf475e868b0
Beckham <bjedev@gmail.com>
2024-02-12 16:15:55 -0500

    Wow... thats sketchy

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,10 @@
 import "@/styles/globals.css";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {Toaster} from "react-hot-toast";
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import Meta from "@/components/MetaComponent";
 import {useThemeStore} from "@/state/basic-state";
 import {Analytics} from "@vercel/analytics/react";
-import {SpeedInsights} from "@vercel/speed-insights/react";
 
 const queryClient = new QueryClient()
 
@@ -19,17 +18,14 @@ export default function App({Component, pageProps}) {
     }, []);
 
     return (
-        <>
-            <QueryClientProvider client={queryClient}>
-                <Toaster/>
-                <Meta/>
-                <div data-theme={themeStore.theme}>
-                    <Component {...pageProps} />
-                </div>
-            </QueryClientProvider>
-
-            <Analytics />
-            <SpeedInsights />
-        </>
+        <QueryClientProvider client={queryClient}>
+            <div className={"bg-success"} /> {/* This is required on each page to fix the buttons not coloring as success properly */}
+            <Toaster/>
+            <Meta/>
+            <div data-theme={themeStore.theme}>
+                <Component {...pageProps} />
+            </div>
+            <Analytics/>
+        </QueryClientProvider>
     );
 }
